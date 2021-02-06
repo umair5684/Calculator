ae04f6f34814c5e068ec71aab1aa54b96f896ba4
umair5684 <muhammadumair5684@gmail.com>
2021-02-06 15:37:15 +0500

    Calculator

---
+++ calculator.js
@@ -0,0 +1,10 @@
+let num='';
+function myFunction(value){
+     num += value;;
+     document.getElementById("demo").value = num;
+}
+function myFunction(symbol){
+    num += symbol;;
+    document.getElementById("demo").value = num;
+    
+}
